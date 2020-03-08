daa10061ca55d2c9578edfc274b18479ffeebd79
Iron4dam <adamxinyuyang@icloud.com>
2020-03-08 14:07:39 +0000

    trivial changes

--- positionEstimatorTraining.m
+++ positionEstimatorTraining.m
@@ -66,16 +66,16 @@ function [modelParameters] = positionEstimatorTraining(training_data, scale, thr
         
         disp('training model 1')
         tic;
-%         model1 = fitlm(spike_angle, distanceX);
+        model1 = fitlm(spike_angle, distanceX);
 %         model1 = fitrkernel(spike_angle, distanceX);
-        model1 = fitrgp(spike_angle, distanceX);
+%         model1 = fitrgp(spike_angle, distanceX);
         toc
         
         disp('training model 2')
         tic;
-%         model2 = fitlm(spike_angle,distanceY);
+        model2 = fitlm(spike_angle,distanceY);
 %         model2 = fitrkernel(spike_angle,distanceY);
-        model2 = fitrgp(spike_angle,distanceY);
+%         model2 = fitrgp(spike_angle,distanceY);
         toc
 
         modelParameters{angle} = {model1, model2, selected_neurons};
