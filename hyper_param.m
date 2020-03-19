31067c623cd39fccb08ecdd42e79ffddb3ca8afb
Iron4dam <adamxinyuyang@icloud.com>
2020-03-19 02:48:04 +0000

    grid search

--- hyper_param.m
+++ hyper_param.m
@@ -2,7 +2,7 @@ rmse_old = inf;
 for win_len = 1:1:20
     for scale = 0:10:100
         for thres = 0.0:0.01:0.2
-            rmse = testFunction_for_student_MTb(scale,thres,win_len);
+            rmse = testFunction_for_students_MTb(scale,thres,win_len);
             if rmse < rmse_old
                 best_params = {scale,thres,win_len}
             end
